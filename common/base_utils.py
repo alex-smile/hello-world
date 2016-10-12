1c1ddb180f5ca2bba75e10d8ff94dd31403f3034
tianci <tianci@TENCENT64.site>
2016-10-12 14:28:57 +0800

    add base_utils

---
+++ common/base_utils.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+class FancyDict(dict):
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError, k:
+            raise AttributeError(k)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError, k:
+            raise AttributeError(k)
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR', '')
+    return ip
+
+
+def get_local_ip():
+    """Get server inner ip"""
+    try:
+        import socket
+        import fcntl
+        import struct
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', 'eth1')
+        )[20:24])
+    except:
+        return '127.0.0.1'
